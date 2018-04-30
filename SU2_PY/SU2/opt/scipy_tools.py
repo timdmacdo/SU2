214b58b0aab87ff97d7d8dfd4eb8f3caba8c3a96
Tim MacDonald <timdmacdo@gmail.com>
2018-04-30 14:29:36 -0700

    reduced verify level

--- SU2_PY/SU2/opt/scipy_tools.py
+++ SU2_PY/SU2/opt/scipy_tools.py
@@ -167,7 +167,7 @@ def pySNOPT(project,x0=None,xb=None,its=100,accu=1e-12,grads=True):
     grad_function_final = lambda x,f,g:grad_function_base(x,f,g,project)        
     
     opt.setOption('Function precision', accu)
-    opt.setOption('Verify level',3)
+    opt.setOption('Verify level',0)
     opt.setOption('Major optimality tolerance',eps)
     outputs = opt(opt_prob, sens_type=grad_function_final)
             
